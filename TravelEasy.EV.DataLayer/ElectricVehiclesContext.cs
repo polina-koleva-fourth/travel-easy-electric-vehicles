16700fcccc03bb5d2a53acacbe5cf0a584fbe16c
polina-koleva-fourth <polina.koleva@fourth.com>
2023-06-30 17:10:18 +0300

    Added the initial solution with Datalayer project and Web API, gitignore

---
+++ TravelEasy.EV.DataLayer/ElectricVehiclesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEasy.EV.DataLayer.Models;
+
+namespace TravelEasy.EV.DataLayer
+{
+    public class ElectricVehiclesContext : DbContext
+    {
+        public DbSet<ElectricVehicle> ElectricVehicles { get; set;}
+
+        public ElectricVehiclesContext(DbContextOptions<ElectricVehiclesContext> options)
+           : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EasyTravel;Trusted_Connection=True");
+        //}
+    }
+}
